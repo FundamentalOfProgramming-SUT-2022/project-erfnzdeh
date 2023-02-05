940b01e580bc0d095800c227c6f9090114986cd4
Pourya Erfanzadeh <erfnzdeh@gmail.com>
2023-02-05 06:50:27 +0330

    minor changes in vim library file.

--- functions.h
+++ functions.h
@@ -5,6 +5,17 @@ typedef long long ll;
 #define TRUE  1
 #define FALSE 0
 
+char clipboard[10000] = {'\0'};
+
+int isFileExists(const char *path)
+{
+    // Check for file existence
+    if (access(path, F_OK) == -1)
+        return 0;
+    return 1;
+}
+
+/* Didn't use this shit. */
 char* remove_leading_slash(char* address) { // credit: Heokhe
     if (address[0] != '/') return address;
     int len = strlen(address);
