025b9bb911d50157275b8123b4ee0ccd7044be57
Pourya Erfanzadeh <erfnzdeh@gmail.com>
2023-02-05 06:49:02 +0330

    pastestr.h file added. nothing yet.

---
+++ pastestr.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "string.h"
+
+typedef long long ll;
+#define TRUE  1
+#define FALSE 0
+
+void print_message(char *message);
+
+void pastestr() {
+    printf("command = pastestr\n");
+}
